098714a2292f059c48ea84c3d4c091151810fa67
palakag123 <palakagarwal1237@gmail.com>
2022-03-23 15:23:17 +0530

    feat: services to get restaurants data

---
+++ services/restaurants.services.js
@@ -0,0 +1,8 @@
+const db = require("../models");
+
+const getRestaurants = async () => {
+  const restaurants = await db.Restaurants.findAll();
+  return restaurants;
+};
+
+module.exports = { getRestaurants };

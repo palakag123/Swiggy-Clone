23865d3db22ce42dbde5a4977452752c82e7d64f
Sara-Jain <Sara_Jain@mckinsey.com>
2022-03-23 14:28:50 +0530

    feat: seed the db

---
+++ seeders/20220323084430-add-restaurants.js
@@ -0,0 +1,56 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Restaurants', [
+      {
+        fullName: 'Restaurant A',
+        costForTwo: 200,
+        Location: 'Bangalore',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+
+      {
+        fullName: 'Restaurant B',
+        costForTwo: 2000,
+        Location: 'Bangalore',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+
+      {
+        fullName: 'Restaurant C',
+        costForTwo: 1500,
+        Location: 'Bangalore',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+
+      {
+        fullName: 'Restaurant D',
+        costForTwo: 15000,
+        Location: 'Bangalore',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Restaurants', null, {});
+  },
+};
